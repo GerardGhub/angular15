c7fd313d2e6c89b2cb19d8144605494306570fac
GerardGhub <singian28@gmail.com>
2024-01-13 16:32:15 +0800

    Event binding and property refresk KT

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+
+  constructor() {}
+
+  ngOnInit(): void {
+
+  }
+
+  headername = "Angular bakla"
+
+  salary = 0.45;
+
+ 
+
+  Functionclick(name:string) {
+    alert(name);
+  }
 
 }
